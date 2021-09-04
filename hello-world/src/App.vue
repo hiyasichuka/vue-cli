aa50b4e770bd105379f97bd66bd903df21f7ec4e
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-09-05 07:01:04 +0900

    vue add vuetify

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,42 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <template>
-      <like-header></like-header>
-      <h2>{{ number }}</h2>
-      <LikeNumber v-bind:totalNumber="number"></LikeNumber>
-      <LikeNumber :total-number="number"></LikeNumber>
-    </template>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// component ローカル登録
-import HelloWorld from "./components/HelloWorld.vue";
-import LikeHeader from "./components/LikeHeader.vue";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "App",
-  data() {
-    return {
-      number: 16,
-    };
-  },
+  name: 'App',
+
   components: {
     HelloWorld,
-    LikeHeader,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 //import App from './App.vue'
 import App from './App.vue'
 import LikeNumber from './components/LikeNumber'
+import vuetify from './plugins/vuetify'
 // お助けヘルプ。trueにした場合、デベロッパーツールで確認できる。
 Vue.config.productionTip = false
 // component グローバル登録
@@ -11,7 +12,9 @@ Vue.component('LikeNumber', LikeNumber)
 console.log(App);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+
   // render: function(h){ return h(App)} でもOK
+  render: h => h(App)
 }).$mount('#app')
 // appはpublic/index.htmlにマウントされている。
